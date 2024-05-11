6425b9a05053c24567dc419e4c2dd824426ea231
Maximilian MxH. Hilmbauer <mxh@mxh-networks.cloud>
2024-05-11 22:27:27 +0200

    feat: add problem 0009 java solution

---
+++ 0009 - Palindrome Number/Solution - Java/src/test/java/SolutionTest.java
@@ -0,0 +1,27 @@
+import maxhilmbauer.sew.Solution;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SolutionTest {
+
+    private final Solution solution = new Solution();
+
+    @Test
+    void isPalindromeCase1() {
+        assertTrue(solution.isPalindrome(121));
+    }
+
+    @Test
+    void isPalindromeCase2() {
+        assertFalse(solution.isPalindrome(-121));
+
+    }
+
+    @Test
+    void isPalindromeCase3() {
+        assertFalse(solution.isPalindrome(10));
+
+    }
+
+}
